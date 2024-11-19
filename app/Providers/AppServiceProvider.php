161e7a746dfdc1b65fbec4da942dfea07aab8c51
Dhiren Jaypal <dhirennjaypal@gmail.com>
2024-11-19 22:06:41 +0530

    Added pagination [Day 14]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Providers;
-
+;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+        // Paginator::useBootstrapFive();
     }
 }
