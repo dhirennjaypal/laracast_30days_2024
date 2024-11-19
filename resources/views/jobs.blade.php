ba5e14afd7a83656be5ccefe42c5da5ea2f13f05
Dhiren Jaypal <dhirennjaypal@gmail.com>
2024-11-19 21:49:57 +0530

    Improved job listing, added eager loading and disabled lazy loading [Day 13]

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,14 @@
     <x-slot:header>
         Job Listing
     </x-slot:header>
-    <ul>
-    @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{ $job->id }}">
-                <strong>{{ $job->title }}</strong> {{ $job->salary }}
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job->id }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+				<div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                	<strong>{{ $job->title }}</strong> {{ $job->salary }}
+				</div>
             </a>
-        </li>
-    @endforeach
-    </ul>
+        @endforeach
+    </div>
 </x-layout>
