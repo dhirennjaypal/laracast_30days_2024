00744138c23be7387ef72f9688f68d4ac9492ae3
Dhiren Jaypal <dhirennjaypal@gmail.com>
2024-11-20 22:31:36 +0530

    Added some validations [Day 17]

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,8 +16,12 @@
 						<label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
 						<div class="mt-2">
 							<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-								<input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="Web Developer">
+								<input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="Web Developer" minlength="3" required>
 							</div>
+
+                            @error("title")
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
 						</div>
 					</div>
 
@@ -25,11 +29,25 @@
 						<label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
 						<div class="mt-2">
 							<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-								<input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="₹6,00,000/Year">
+								<input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="₹6,00,000/Year" required>
 							</div>
+
+                            @error("salary")
+                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
 						</div>
 					</div>
 				</div>
+
+                {{-- <div class="mt-10">
+                    @if( $errors->any() )
+                        <ui>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ui>
+                    @endif
+                </div> --}}
 			</div>
 		</div>
 
