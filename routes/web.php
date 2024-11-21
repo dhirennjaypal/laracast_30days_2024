12042380f350799259800b8ec2bccadcd214dbce
Dhiren Jaypal <dhirennjaypal@gmail.com>
2024-11-21 22:21:25 +0530

    Simplified route model relationship, grouping, etc... [Day 19]

--- routes/web.php
+++ routes/web.php
@@ -1,77 +1,32 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/laravel', function () {
-    return view('welcome');
-});
+Route::view('/laravel', "welcome");
+Route::view('/', "home");
+Route::resource("jobs", JobController::class);
 
-Route::get('/', function () {
-    return view('home');
-});
-
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with("employer")->latest()->paginate(20);
-    return view('jobs.index', [
-        "jobs" => $jobs
-    ]);
-});
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::get('/jobs/{id}', function ($id) {
-    return view('jobs.show', [
-        "job" => Job::find($id)
-    ]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    request()->validate([
-        "title"     =>	[ "required", "min:3" ],
-        "salary"    =>	[ "required" ],
-    ]);
-
-    Job::create([
-        "title"			=>	request("title"),
-        "salary"		=>	request("salary"),
-        "employer_id"	=>	1,
-	]);
-
-    return redirect("/jobs");
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    return view('jobs.edit', [
-        "job" => Job::find($id)
-    ]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        "title"     =>	[ "required", "min:3" ],
-        "salary"    =>	[ "required" ],
-    ]);
-
-	$job			=	Job::findOrFail($id);
-	$job->update([
-		"title"		=>	request("title"),
-		"salary"	=>	request("salary"),
-	]);
-
-	return redirect("/jobs/{$job->id}");
-});
-
-// Delete
-Route::delete('/jobs/{id}', function ($id) {
-	Job::findOrFail($id)->delete();
-	return redirect("/jobs");
-});
+// Route::get('/laravel', function () {
+//     return view('welcome');
+// });
+// Route::controller(JobController::class)->group(function(){
+// 	Route::get('/jobs', "index");
+// 	Route::get('/jobs/create', "create");
+// 	Route::get('/jobs/{job}', "show");
+// 	Route::post('/jobs', "store");
+// 	Route::get('/jobs/{job}/edit', "edit");
+// 	Route::patch('/jobs/{job}', "update");
+// 	Route::delete('/jobs/{job}', "destroy");
+// });
+// Route::get('/jobs/{job}', [JobController::class, "show"]);
+// Route::get('/jobs/{job}', function (Job $job) {
+//     return view('jobs.show', [
+//         "job" => $job
+//     ]);
+// });
+// Route::get('/jobs/{id}', function ($id) {
+//     return view('jobs.show', [
+//         "job" => Job::find($id)
+//     ]);
+// });
